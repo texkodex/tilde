f639cee171b08908f00dfc17988454d22d601755
texkodex <texkodex@gmail.com>
2018-07-19 16:10:03 -0400

    added switch navigation

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { createSwitchNavigator } from 'react-navigation';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,4 +9,57 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  home: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 25,
+    borderColor: 'lightblue',
+  },
+  login: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 25,
+    borderColor: 'lightgreen',
+  },
 });
+
+class ScreenComponentHome extends React.Component {
+  render() {
+    return (
+      <View style={styles.home}>
+        <Button 
+          title="Login"
+          onPress={() => this.props.navigation.navigate("Login")}
+        />
+      </View>
+    );
+  }
+}
+
+class ScreenComponentLogin extends React.Component {
+  render() {
+    return (
+      <View style={styles.login}>
+        <Button 
+          title="Home"
+          onPress={() => this.props.navigation.navigate("Home")}
+        />
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createSwitchNavigator({
+  "Home": ScreenComponentHome,
+  "Login": ScreenComponentLogin,
+});
+
+export default class App extends React.Component {
+  render() {
+    return <AppNavigator />;
+  }
+}
+
+
